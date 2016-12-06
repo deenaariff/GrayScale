34a33b34dac12f42d75badb779dd1cf7c3b497d0
daariff <aariff.deen@gmail.com>
2016-12-06 01:05:05 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Convert Images to GreyScale
+
+A small script to convert any image to greyscale through averaging RGB values in a Three Dimensional Array of the image.
+
+## Usage
+
+`python grey.py <image.filextension>`
---
+++ grey.py
@@ -0,0 +1,32 @@
+from scipy import misc
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt 
+import sys.argv as argv
+
+
+image = misc.imread(argv[1])
+
+# the tuple shape (row, columns, <rgb vector>)
+print image.shape
+
+# Intialize a new array of zeroes with the same shape
+grey = np.zeros((image.shape[0],image.shape[1]));
+grey2 = np.zeros((image.shape[0],image.shape[1]));
+
+# 'Human' Average - adapted for human eyes
+def average1(pixel):
+    return (0.299*pixel[0] + 0.587*pixel[1] + 0.114*pixel[2])/3
+
+# Raw Average 
+def average2(pixel):
+    return np.average(pixel);
+
+# Map averages of pixels to the grey image
+for r in range(len(image)): 
+    for c in range(len(image[r])): 
+        # Use human average
+        grey[r][c] = average1(image[r][c]);
+
+plt.imshow(grey, cmap = matplotlib.cm.Greys_r) 
+plt.show()
